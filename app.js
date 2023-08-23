2e00888f5c5904fdffd12a06544808bacbdea281
LIMING ZHENG <zlm17unl@gmail.com>
2023-08-23 13:04:32 -0400

    123

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(cors(
     {
         credentials: true,
-        origin: 'http://localhost:3000'
+        origin: process.env.FRONTEND_URL
         // 'http://localhost:3000'
         // process.env.FRONTEND_URL
     }
